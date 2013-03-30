bcd3f4b30ac3531461aac341589b45c215fc44c3
Warren Eaton <warren.eaton@alumnos.usm.cl>
2013-03-30 15:58:17 -0400

    tarea1: update main.c

--- tareas/tarea1/main.c
+++ tareas/tarea1/main.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
-#include "evento.h"
+//#include "evento.h"
+
+void error(char * op, int param)
+{
+	if(!strcmp(op,"invalid"))
+		printf("error: invalid parameter\n");
+	else
+		printf("error: operation \"%s\" needs %d parameter%c\n", op, param, param != 1 ? 's' : ' ');
+}
 
 int main(int argc, char * argv[])
 {
+	// lack of parameters
+	if(argc == 1)
+	{
+		error("invalid",0);
+		return 1;
+	}
 
 	if(!strcmp(argv[1], "new"))
 	{
-
+		if(argc != 4)
+		{
+			error("new", 2);
+			return 1;
+		}
 	}
 	else if(!strcmp(argv[1], "get"))
 	{
-
+		if(argc!= 3)
+		{
+			error("get", 1);
+			return 1;
+		}
 	}
 	else if(!strcmp(argv[1], "delete"))
 	{
-
+		if(argc!= 3)
+		{
+			error("delete", 1);
+			return 1;
+		}
 	}
 	else if(!strcmp(argv[1], "update"))
 	{
@@ -24,19 +50,34 @@ int main(int argc, char * argv[])
 	}
 	else if(!strcmp(argv[1], "list"))
 	{
+		if(argc!= 2)
+		{
+			error("list", 0);
+			return 1;
+		}
 
 	}
 	else if(!strcmp(argv[1], "clear"))
 	{
+		if(argc!= 2)
+		{
+			error("clear", 0);
+			return 1;
+		}
 
 	}
 	else if(!strcmp(argv[1], "do"))
 	{
-
+		if(argc!= 3)
+		{
+			error("do", 1);
+			return 1;
+		}
 	}
 	else
 	{
-
+		error("invalid", 0);
+		return 1;
 	}
 	
 	return 0;
