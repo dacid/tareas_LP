fee48f1b126a2f76e7362a6aecc21a92d0fedf63
Warren Eaton <warren.eaton@alumnos.usm.cl>
2013-03-30 13:08:23 -0400

    create folders and files

---
+++ tareas/tarea1/evento.c
@@ -0,0 +1,37 @@
+#include "evento.h"
+
+void evento::nuevo_evento(evento * ptr_evento)
+{
+
+}
+
+void evento::mostrar_evento(evento * ptr_evento)
+{
+
+}
+
+void evento::eliminar_evento(evento * ptr_evento)
+{
+
+}
+
+void evento::modificar_evento(evento * ptr_evento)
+{
+
+}
+
+void evento::concretar_evento(evento * ptr_evento)
+{
+
+}
+
+void evento::listar_eventos()
+{
+
+}
+
+void evento::vaciar_eventos()
+{
+
+}
+
---
+++ tareas/tarea1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "evento.h"
+
+int main(int argc, char * argv[])
+{
+
+	switch()	
+	
+	return 0;
+}
